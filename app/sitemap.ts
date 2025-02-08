51b9b2280e9fcc9dcfe956d59e599c82e736c4c8
lemajr <erickbale360@gmail.com>
2025-02-08 21:21:08 +0300

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,25 @@
+import { fetchProperties } from "@/lib/data";
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const properties = await fetchProperties();
+
+  return [
+    {
+      url: "https://www.blackwill.co.tz/",
+      lastModified: new Date().toISOString(),
+    },
+    {
+    url: "https://www.blackwill.co.tz/contact",
+    lastModified: new Date().toISOString(),
+    },
+    {
+      url: "https://www.blackwill.co.tz/listing",
+      lastModified: new Date().toISOString(),
+    },  
+    ...properties.map((property) => ({
+      url: `https://www.blackwill.co.tz/listing/${property.id}`,
+      lastModified: property.updated_at || new Date().toISOString(),
+    })),
+  ];
+}
