fa7f2d407b5fe0522a1b527f63dae350928b7287
lemajr <erickbale360@gmail.com>
2025-03-04 22:38:37 +0300

    fix typo

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -4,11 +4,12 @@ const ContactForm = () => {
   return (
     <div className="flexCenter">
       <div className="max-padd-container ">
-        <h2 className="h2">Contact our team</h2>
-        <h5 className="medium-18">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab quos a
-          unde! Corporis iure vitae deserunt nobis veritatis voluptatem quaerat.
-        </h5>
+      <h2 className="h2">Contact Our Team</h2>
+<h5 className="medium-18">
+  Looking to buy, sell, or rent a property? Our experts are here to assist you every step of the way.  
+  Get in touch with us for personalized real estate solutions.
+</h5>
+
       </div>
     </div>
   );
